9422e6a201a2b8adf79aeb967966b176fbd475e0
Chris LL <aerdeyn@gmail.com>
2020-05-02 15:46:41 +1000

    Updates

---
+++ precache-manifest.6ffccfc2dcc8222465932f7a30232cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e36c7791e613ca03c61",
+    "url": "/robofriends/static/css/main.61592f70.chunk.css"
+  },
+  {
+    "revision": "4e36c7791e613ca03c61",
+    "url": "/robofriends/static/js/main.4e36c779.chunk.js"
+  },
+  {
+    "revision": "110edcf59a7571f184c1",
+    "url": "/robofriends/static/css/1.b854ed97.chunk.css"
+  },
+  {
+    "revision": "110edcf59a7571f184c1",
+    "url": "/robofriends/static/js/1.110edcf5.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "08defda11be47a678766880077fd7016",
+    "url": "/robofriends/index.html"
+  }
+];
